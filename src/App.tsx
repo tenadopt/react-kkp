4c614a3fc3a0eb7b38f0919dc77bad4a89f998cd
Tenadopt <yury.ignatiev@gmail.com>
2023-06-13 11:38:54 +0300

    add components

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,71 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    debugger
+    console.log("App rendering");
+    return (
+        <div>
+            This is APP component
+            <Rating/>
+            <Accordion/>
+        </div>
+    );
+}
+
+function Rating() {
+    debugger
+    console.log("Rating rendering");
+    return (
+        <>
+        <Star/>
+        <Star/>
+        <Star/>
+        <Star/>
+        <Star/>
+        </>
+    )
+}
+
+const Accordion = () => {
+    debugger
+    console.log("Accordion rendering");
+    return (
+        <div>
+            <AccordionTitle/>
+            <AccordionBody/>
+        </div>
+    )
+}
+
+const AccordionTitle = () => {
+    debugger
+    console.log("AccordionTitle rendering");
+    return (
+        <h3>Меню</h3>
+    )
+}
+
+const AccordionBody = () => {
+    debugger
+    console.log("AccordionBody is rendering");
+    return (
+        <div>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </div>
+    )
+}
+
+
+const Star = () => {
+    debugger
+    console.log("Star is rendering");
+    return (
+        <div>
+            star
+        </div>
+    )
 }
 
 export default App;
